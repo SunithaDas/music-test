c7d67a5f94f6f3af66307d2d6540166735fbd129
SunithaDas <76248297+SunithaDas@users.noreply.github.com>
2020-12-27 23:14:07 +0530

    Update index.ts
    music test solution

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,7 +2,94 @@ import { notesToPlayInOrder } from "./music-to-play";
 
 function playMusic() {
     const notes = notesToPlayInOrder;
-    
+     // TODO Play these notes one after the other at the pitch and rhythm given in each note
+   for (i=0; i<notes.length; i++)
+	{
+	if(MusicalNote.pitch==A && MusicalNote.octave==4)
+	{
+		
+		var x= document.getElementById("A4")
+		x. play();
+		
+	}
+	
+	else if(MusicalNote.pitch==A && MusicalNote.octave==5)
+	{ 
+       
+		var x= document.getElementById("A5")
+		x. play();
+	
+	}
+	else if(MusicalNote.pitch==B && MusicalNote.octave==4 && MusicalNote.accidental== Accidental.FLAT)
+	{
+		
+		var x= document.getElementById("B4F")
+		x. play();
+	
+	}
+	else if(MusicalNote.pitch==B && MusicalNote.octave==5 && MusicalNote.accidental== Accidental.FLAT)
+	{   
+          
+		var x= document.getElementById("B5F")
+		x. play();
+		
+	}
+	else if(MusicalNote.pitch==C && MusicalNote.octave==5)
+	{   
+		var x= document.getElementById("C5")
+		x. play();
+	}
+	
+	else if(MusicalNote.pitch==D && MusicalNote.octave==4)
+	{  
+		var x= document.getElementById("D4")
+		x. play();
+	
+	}
+	else if(MusicalNote.pitch==D && MusicalNote.octave==5 && MusicalNote.accidental== Accidental.SHARP )
+	{  
+		var x= document.getElementById("D5S")
+		x. play();
+	
+	}
+	
+	else if(MusicalNote.pitch==D && MusicalNote.octave==5)
+	{  
+		var x= document.getElementById("D5")
+		x. play();
+	
+	}
+	else if(MusicalNote.pitch==F && MusicalNote.octave==4)
+	{  
+		var x= document.getElementById("F4")
+		x. play();
+	}
+	if(MusicalNote.pitch==F && MusicalNote.octave==5 && MusicalNote.accidental== Accidental.SHARP )
+	{   
+		var x= document.getElementById("F5S")
+		x. play();
+	
+	}
+	else if(MusicalNote.pitch==F && MusicalNote.octave==5)
+	{   
+		var x= document.getElementById("F5")
+		x. play();
+	}
+	else if(MusicalNote.pitch==G && MusicalNote.octave==4)
+	{  
+		var x= document.getElementById("G4")
+		x. play();
+
+	}
+	else (MusicalNote.pitch==G && MusicalNote.octave==5)
+	{   
+		var x= document.getElementById("G5")
+		x. play();
+
+	}
+}	
+}
+ 
 
 }
 
